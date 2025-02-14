f457b430bbacc7f39d2a46da455341bba54c03a3
Subodh Kushwaha <subodhkushwaha36@gmail.com>
2025-02-14 20:09:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function animateName(name = " Wish you very Happy Birthday, Aditya Patel! May this special day bring you joy, success, and endless happiness.", speed = 1000, loop = false) {
+function animateName(name = "   Wish you very Happy Birthday, Aditya Patel! May this special day bring you joy, success, and endless happiness.", speed = 1000, loop = false) {
     const nameElement = document.querySelector(".logo h2");
     if (!nameElement) return; // Check if the element exists
   
@@ -27,4 +27,4 @@ function animateName(name = " Wish you very Happy Birthday, Aditya Patel! May th
 }
 
 // Call the function to start the animation
-animateName( " Wish you very Happy Birthday, Aditya Patel! May this special day bring you joy, success, and endless happiness.", 250, true); // (Name, Speed, Loop)
+animateName( "   Wish you very Happy Birthday, Aditya Patel! May this special day bring you joy, success, and endless happiness.", 250, true); // (Name, Speed, Loop)
